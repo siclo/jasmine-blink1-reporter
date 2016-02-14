160db1aa853754d2788e29bd93861434b8ba292c
Simon Cloutier <simon.cloutier@gmail.com>
2016-02-13 22:58:30 -0500

    Fade to yellow at the start in case the suite is long. Switch to red on failure

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 'use strict';
 
+var Blink1 = require('node-blink1');
+
 module.exports = function (options) {
+    var self = this;
+
     this.jasmineStarted = function (suiteInfo) {
-        console.log("Blink1!", suiteInfo);
+        try {
+            self.blink1 = new Blink1();
+            self.yellow();
+            console.log("Blink1!", suiteInfo);
+        } catch(e) {
+            console.log(e);
+        }
+    };
+
+    this.specDone = function (result) {
+        console.log(result);
+        if (result.failedExpectations.length > 0) {
+            self.red();
+        }
+    };
+
+    this.yellow = function () {
+        self.blink1.fadeToRGB(100, 155, 155, 0);
+    };
+
+    this.red = function () {
+        self.blink1.setRGB(255, 0, 0);
     };
 };
 
